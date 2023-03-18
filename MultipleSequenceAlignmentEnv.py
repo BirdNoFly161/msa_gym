8af1303b9d4a4b7cf3c2e8579fe8f970437ffd02
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2023-03-18 22:52:19 +0100

    faster state management

--- MultipleSequenceAlignmentEnv.py
+++ MultipleSequenceAlignmentEnv.py
@@ -7,11 +7,15 @@ def getblosum62():
 
 class MultipleSequenceAlignmentEnv(gym.Env):
     alphabet = 'ARNDCEQGHILKMFPSTYWV' # 20 amino acids also works for DNA/RN
-    onehot_transform = dict(zip(alphabet, range(len(alphabet))))
+    onehot = np.eye(len(alphabet)) # binary encoding of the alphabet
+    onehot_transform = dict(zip(alphabet, range(len(alphabet)))) # dictionary to transform letters to indices
+
     weight_matrix = getblosum62()
     def __init__(self, sequences):
         super(MultipleSequenceAlignmentEnv, self).__init__()
+        self.score = 0
         self.state = None
+        self.one_hot_sequences = None
         self.sequences = sequences
         self.n_sequences = len(sequences)
         self.n_characters = len(self.alphabet) 
@@ -19,59 +23,55 @@ class MultipleSequenceAlignmentEnv(gym.Env):
         self.action_space = gym.spaces.MultiDiscrete([self.n_sequences, self.max_length])
         self.observation_space = gym.spaces.Dict({
             'one_hot_sequences' : gym.spaces.MultiBinary([self.n_sequences, self.max_length, self.n_characters]),
-            'positions' : gym.spaces.MultiDiscrete([self.n_sequences, self.max_length])
-            # profiles could be a good idea
-            #'profiles' : gym.spaces.MultiBinary([self.max_length, self.n_characters])
+            'positions' : gym.spaces.MultiDiscrete([self.n_sequences, self.max_length]),
+            # profile could be a good idea for the model to know the distribution of amino acids in each position 
+            #'profile' : gym.spaces.MultiBinary([self.max_length, self.n_characters])
         })
         
         
 
     def reset(self):
-        self.state = np.full((self.n_sequences, self.max_length), '-', dtype='<U1') # Fill the state with gap symbols
-        for i, seq in enumerate(self.sequences):
-            self.state[i, :len(seq)] = list(seq) # Add the original sequences to the state
+        self.state = np.zeros([self.n_sequences, self.max_length], dtype=np.int_) # watch out for overflow
+        self.one_hot_sequences = np.zeros([self.n_sequences, self.max_length, self.n_characters], dtype=np.int8) # too big for binary
+        for i, sequence in enumerate(self.sequences):
+            # one hot encoding of the sequences
+            self.one_hot_sequences[i, :len(sequence), :] = np.array([self.onehot[self.onehot_transform[letter]] for letter in sequence])
+            # state is just the indices of each amino acide which is 1,2,3,4...len(sequence)+1
+            self.state[i, :len(sequence)] = np.arange(1, len(sequence)+1)
+        self.score = self.column_score()
         return self._get_observation()
+            
+
 
     def step(self, action):
         seq_idx, pos = action
         self._insert_gap(seq_idx, pos)
 
         reward = self._calculate_reward()
-        done = False
+        self.score += reward
+        done = False # MSA is an episodic task but we don't know when it ends
         info = {}
 
         return self._get_observation(), reward, done, info
 
     def _get_observation(self):
-        obs = np.zeros(self.observation_shape, dtype=int)
-        for i, row in enumerate(self.state):
-            for j, aa in enumerate(row):
-                obs[i, j, self.letter_2_index(aa)] = 1
-        return obs.flatten()
+        return {
+            'one_hot_sequences' : self.one_hot_sequences,
+            'positions' : self.state,
+            #'profile' : self.profile
+        }
 
-    def letter_2_index(self, letter):
-        print(letter)
-        return self.alphabet.index(letter.upper())
 
     def _insert_gap(self, seq_idx, pos):
-        self.state[seq_idx, pos+1:] = np.roll(self.state[seq_idx, pos+1:], 1)
-        self.state[seq_idx, pos] = '-'
+        # inserting a gap in the sequence seq_idx at position pos
+        self.state[seq_idx, pos:len(self.sequences[seq_idx])] += 1
 
     def _calculate_reward(self):
-        reward = 0
-        for col_idx in range(self.max_length):
-            col = self.state[:, col_idx]
-            col_score = 0
-
-            for i in range(self.n_sequences):
-                for j in range(i+1, self.n_sequences):
-                    pair = tuple(sorted(col[i] + col[j]))
-                    if pair in self.weight_matrix.index:
-                        col_score += self.weight_matrix.loc[pair]# may not work as blosum62 is a dataframe
-
-            reward += col_score
-
-        return reward
+        current_score = self.column_score()
+        return current_score - self.score # reward is the difference between the current score and the previous score
+    
+    def column_score(self):
+        return NotImplementedError
 
     def print_alignment(self):
         for row in self.state:
