be16781b28644747efca108527c110ed47c22835
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2023-03-18 22:23:51 +0100

    efficiency improvement in progress

--- MultipleSequenceAlignmentEnv.py
+++ MultipleSequenceAlignmentEnv.py
@@ -6,18 +6,25 @@ def getblosum62():
     return pd.read_csv('blosum62.csv', index_col=0)
 
 class MultipleSequenceAlignmentEnv(gym.Env):
+    alphabet = 'ARNDCEQGHILKMFPSTYWV' # 20 amino acids also works for DNA/RN
+    onehot_transform = dict(zip(alphabet, range(len(alphabet))))
+    weight_matrix = getblosum62()
     def __init__(self, sequences):
         super(MultipleSequenceAlignmentEnv, self).__init__()
+        self.state = None
         self.sequences = sequences
         self.n_sequences = len(sequences)
-        self.n_characters = 25 # 20 amino acids + 4 for DNA/RNA + 1 for gap
-        
-        self.max_length = max(len(sequence) for sequence in sequences) + 1 # Add 1 to account for gap insertions
+        self.n_characters = len(self.alphabet) 
+        self.max_length = max(len(sequence) for sequence in sequences)
         self.action_space = gym.spaces.MultiDiscrete([self.n_sequences, self.max_length])
-        self.observation_shape = (self.n_sequences, self.max_length, self.n_characters)
-        self.observation_space = gym.spaces.MultiDiscrete([self.n_characters] * self.n_sequences * self.max_length)
-        self.blosum62 = getblosum62()
-        self.state = None
+        self.observation_space = gym.spaces.Dict({
+            'one_hot_sequences' : gym.spaces.MultiBinary([self.n_sequences, self.max_length, self.n_characters]),
+            'positions' : gym.spaces.MultiDiscrete([self.n_sequences, self.max_length])
+            # profiles could be a good idea
+            #'profiles' : gym.spaces.MultiBinary([self.max_length, self.n_characters])
+        })
+        
+        
 
     def reset(self):
         self.state = np.full((self.n_sequences, self.max_length), '-', dtype='<U1') # Fill the state with gap symbols
@@ -39,15 +46,12 @@ class MultipleSequenceAlignmentEnv(gym.Env):
         obs = np.zeros(self.observation_shape, dtype=int)
         for i, row in enumerate(self.state):
             for j, aa in enumerate(row):
-                obs[i, j, self._aa_to_index(aa)] = 1
+                obs[i, j, self.letter_2_index(aa)] = 1
         return obs.flatten()
 
-    def _aa_to_index(self, aa):
-        aa = aa.upper()
-        if aa == '-':
-            return 20
-        else:
-            return ord(aa) - ord('A')
+    def letter_2_index(self, letter):
+        print(letter)
+        return self.alphabet.index(letter.upper())
 
     def _insert_gap(self, seq_idx, pos):
         self.state[seq_idx, pos+1:] = np.roll(self.state[seq_idx, pos+1:], 1)
@@ -62,8 +66,8 @@ class MultipleSequenceAlignmentEnv(gym.Env):
             for i in range(self.n_sequences):
                 for j in range(i+1, self.n_sequences):
                     pair = tuple(sorted(col[i] + col[j]))
-                    if pair in self.blosum62.index:
-                        col_score += self.blosum62.loc[pair]# may not work as blosum62 is a dataframe
+                    if pair in self.weight_matrix.index:
+                        col_score += self.weight_matrix.loc[pair]# may not work as blosum62 is a dataframe
 
             reward += col_score
 
@@ -84,5 +88,6 @@ if __name__ == "__main__":
     obs = env.reset()
 
     action = env.action_space.sample()
+    print(action)
     obs, reward, done, info = env.step(action)
     env.print_alignment()
